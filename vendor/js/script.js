2e0e7d4e79b28e45d814c42e15b4d210cc4273b7
Vadim Litau <vadimlitau@yandex.ru>
2021-10-17 22:56:01 +0600

    прикрутил к карте js и открытие попап, изменение цвета области при наведении на кнопку

--- vendor/js/script.js
+++ vendor/js/script.js
@@ -1,1 +1,14 @@
+//Открытие попап карты - Коми
+const komi = document.querySelector('.map__komi-button');
+const komiMap = document.querySelector('.map__komi')
+const komiPopup = document.querySelector('.map__popup');
+komi.addEventListener('click', () => {
+    komiPopup.classList.toggle('map__popup_show')
+})
 
+komi.addEventListener('mouseover', () => {
+    komiMap.style.fill = '#6d097a';
+});
+komi.addEventListener('mouseout', () => {
+    komiMap.style.fill = '#9a34a8';
+});

0e1f5539bb29ea570852aa818221c9543e43a6d8
FabioTw <e.cherwoo@gmail.com>
2021-10-17 19:29:52 +0300

    feat: добавить анимация пазлов при скролле

---
+++ scripts/script.js
@@ -0,0 +1,58 @@
+let isScrolling = false;
+
+window.addEventListener("scroll", throttleScroll, false);
+
+function throttleScroll(e) {
+    if (isScrolling == false ) {
+        window.requestAnimationFrame(function() {
+          dealWithScrolling(e);
+          isScrolling = false;
+        });
+    }
+    isScrolling = true;
+}
+
+document.addEventListener("DOMContentLoaded", dealWithScrolling, false);
+
+let purplePuzzle = document.querySelector(".test__img-purple-puzzle");
+let bigGreenPuzzle = document.querySelector(".test__img-green-puzzle");
+let smallGreenPuzzle = document.querySelector(".definition__img-puzzle");
+
+function dealWithScrolling(e) {
+  if (isPartiallyVisible(smallGreenPuzzle)) {
+    smallGreenPuzzle.classList.add("active");
+  } else {
+    smallGreenPuzzle.classList.remove("active");
+  }
+
+  if (isPartiallyVisible(bigGreenPuzzle)) {
+    bigGreenPuzzle.classList.add("active");
+  } else {
+    bigGreenPuzzle.classList.remove("active");
+  }
+
+  if (isPartiallyVisible(purplePuzzle)) {
+    purplePuzzle.classList.add("active");
+  } else {
+    purplePuzzle.classList.remove("active");
+  }
+}
+
+function isPartiallyVisible(el) {
+  let elementBoundary = el.getBoundingClientRect();
+
+  let top = elementBoundary.top;
+  let bottom = elementBoundary.bottom;
+  let height = elementBoundary.height;
+
+  return ((top + height >= 0) && (height + window.innerHeight >= bottom));
+}
+
+function isFullyVisible(el) {
+  let elementBoundary = el.getBoundingClientRect();
+
+  let top = elementBoundary.top;
+  let bottom = elementBoundary.bottom;
+
+  return ((top >= 0) && (bottom <= window.innerHeight));
+}

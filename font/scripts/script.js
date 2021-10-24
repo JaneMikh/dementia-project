ac16cbe47482636a902419f35ed129f5ae27cb7a
Evgeniya Mikhaylyuk <88273125+JaneMikh@users.noreply.github.com>
2021-10-24 13:15:54 +0300

    fix: добавить и подключить шрифты, слить втки develop и feature/header

--- font/scripts/script.js
+++
@@ -1,6 +0,0 @@
-const headerButton = document.querySelector(".header__button");
-const dropdownMenu = document.querySelector(".dropdown-menu");
-
-headerButton.addEventListener('click', function() {
-  dropdownMenu.classList.toggle("dropdown-menu_click_open")
-})
